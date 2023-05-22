140f11ec457975e342c39c0a05f8830366b32e3f
wannazreena <waneenaahmed@gmail.com>
2023-05-22 12:00:31 +0800

    Add options to combo boxes

--- src/main/java/com/game/roundr/lobby/CreateLobbyController.java
+++ src/main/java/com/game/roundr/lobby/CreateLobbyController.java
@@ -2,9 +2,24 @@ package com.game.roundr.lobby;
 
 import com.game.roundr.App;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 
-public class CreateLobbyController {
+public class CreateLobbyController implements Initializable {
+
+    @FXML
+    private ComboBox<Integer> roundsBox;
+    @FXML
+    private ComboBox<Integer> timeBox;
+    @FXML
+    private ComboBox<Integer> playerBox;
+    @FXML
+    private ComboBox<Integer> wordBox;
 
     @FXML
     private void handleMainMenuButtonClick() throws IOException {
@@ -14,6 +29,23 @@ public class CreateLobbyController {
     @FXML
     private void handleCreateLobbyButtonClick() throws IOException {
         App.setScene("lobby/GameLobby");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // Add options to combo boxes
+        ObservableList<Integer> ol = FXCollections.observableArrayList();
+        for (int i = 0; i < 10; i++) {
+            ol.add(i + 1);
+        }
+        roundsBox.getItems().addAll(ol);
+        wordBox.getItems().addAll(ol);
+        ol.clear();
+        for (int i = 0; i < 60; i++) {
+            ol.add(i + 1);
+        }
+        timeBox.getItems().addAll(ol);
+        playerBox.getItems().addAll(2, 3, 4, 5, 6);
     }
 
 }
