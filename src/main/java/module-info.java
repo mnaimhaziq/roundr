59c0018a1441d9306ce2f8de38603066fa682ac1
nurinarina <94106673+nurinarina@users.noreply.github.com>
2023-06-01 21:55:35 +0800

    modify UI for chat sections

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,7 +6,7 @@ module com.game.roundr {
     opens com.game.roundr to javafx.fxml;
     opens com.game.roundr.lobby to javafx.fxml;
     opens com.game.roundr.game to javafx.fxml;
+    opens com.game.roundr.chat to javafx.fxml;
 
-    
     exports com.game.roundr;
 }
